3f0672cd89515b0582cca65f67ac4580a1a4ce1f
vpanchal1293 <66015575+vpanchal1293@users.noreply.github.com>
2020-06-29 23:24:24 +0530

    Added Recursive method

--- Sorting/Selection_Sort/Selection_Sort.c
+++ Sorting/Selection_Sort/Selection_Sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 void selection_sort(int array[],int len);
+void selection_sort_recursion(int array[],int len);
 
 int main()
 {
@@ -36,6 +37,34 @@ void selection_sort(int array[],int len)
         
     }
 }
+
+void selection_sort_recursion(int array[],int len)
+{
+    if(len == 1)
+    {
+        //if only one element left means full array is sorted 
+        return;
+    }
+    int i = 0, min_index = 0;
+        //Take min_index from 0 to len-1
+        min_index = 0;
+        for(i = 1; i < len; i++)
+        // Find min element form unsroted array (min_index+1 to last element of array)
+        if(array[min_index] > array[i])
+        {
+            //Update min_index to index of smallest element of unsorted array
+            min_index = i;
+        }
+        
+        
+        if(min_index != 0)
+        {
+            // if no Update in min_index then swap the element
+            array[min_index] = array[0] + array[min_index] - (array[0] = array[min_index]);
+        }
+        selection_sort_recursion(array+1 ,len-1);
+}
+
 
 print_array(int array[] , int len)
 {
