1a43ced4b4417317fcdded2aadcb7a4bacb21fb0
vpanchal1293 <66015575+vpanchal1293@users.noreply.github.com>
2020-06-30 19:54:26 +0530

    Added Files for Insertion Sort

---
+++ Sorting/Insertion_Sort/Insertion_Sort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+void Insertion_sort(int array[],int len);
+void Insertion_sort_recursion(int array[],int len);
+void print_array(int array[] , int len);
+int main(void)
+{
+    int array[] = {64, 25, 12, 22, 11}, len = 5; 
+    
+    printf("Before Sorting\n");
+    print_array(array , len);
+    Insertion_sort(array , len);
+    printf("Aftre Sorting\n");
+    
+    print_array(array , len);
+    
+    
+    int array_2[] = {64, 25, 12, 22, 11}; 
+    printf("Before Sorting\n");
+    print_array(array_2 , len);
+    Insertion_sort_recursion(array_2 , len);
+    printf("Aftre Sorting\n");
+    print_array(array_2 , len);
+    return 0;
+}
+
+void Insertion_sort(int array[],int len)
+{
+    int i = 0, j = 0, key = 0;
+    for(i = 0; i < len; i++)
+    {
+        key = array[i];
+        j = i - 1;
+        while(j>=0 && array[j] > key)
+        {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+    }
+}
+
+void Insertion_sort_recursion(int array[],int len)
+{
+    int i = 0, key = 0, j = 0;
+    
+    if(len == 1)
+    {
+        return;
+    }
+    
+    Insertion_sort_recursion(array,len-1);
+    
+    key = array[len - 1];
+    j = len - 2;
+    while(j>=0 && array[j] > key)
+    {
+        array[j+1] = array[j];
+        j--;
+    }
+    array[j+1] = key;
+    
+}
+
+
+void print_array(int array[] , int len)
+{
+    int i = 0;
+    for(i = 0; i < len; i++)
+    printf("%d ",array[i]);
+    printf("\n");
+}
---
+++ Sorting/Insertion_Sort/README.md
@@ -0,0 +1,48 @@
+Insertion Sort
+Here array is devided in to two part, Sorted array and Unsortd array
+
+Take first element of Unsortd array as key
+compare key with sorted array element in highest to lowest order
+- Put key to proper place by sifting element of sorted array.
+
+12, 11, 13, 5, 6
+
+i = 1. 
+Key = 11
+Since 11 is smaller than 12, move 12 and insert 11 before 12
+11, 12, 13, 5, 6
+
+i = 2
+Key = 13
+13 will remain at its position as all elements in A[0..I-1] are smaller than 13
+11, 12, 13, 5, 6
+
+i = 3
+Key = 5
+5 will move to the beginning and all other elements from 11 to 13 
+will move one position ahead of their current position.
+5, 11, 12, 13, 6
+
+i = 4
+Key = 6
+6 will move to position after 5, 
+and elements from 11 to 13 will move one position ahead of their current position.
+5, 6, 11, 12, 13
+
+--------------------
+12, 11, 13, 5, 6
+	--
+11, 12, 13, 5, 6
+		--
+11, 12, 13, 5, 6
+			--
+5, 11, 12, 13, 6
+			   --
+--------------------
+End of sorting--
+5, 6, 11, 12, 13
+
+insertionSort(arr, n) 
+    Loop from i = 1 to n-1.
+       a) Pick element arr[i] and insert
+          it into sorted sequence arr[0..i-1] 
